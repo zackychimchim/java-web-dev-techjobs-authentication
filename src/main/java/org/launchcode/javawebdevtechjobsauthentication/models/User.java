8b2344879aa6ac5bdb797609fbfdc9224395608d
Zachary Ainsworth <zacharyainsworth@Zachary-Ainsworths-MacBook-Air.local>
2020-11-02 19:31:48 -0600

    Finished Studio

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -0,0 +1,38 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+import com.sun.istack.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+
+@Entity
+    public class User extends AbstractEntity {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+
+    public User() {}
+
+        public User(String username, String password) {
+            this.username = username;
+            this.pwHash = encoder.encode(password);
+        }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+        public String getUsername() {
+            return username;
+        }
+
+
+
+    }
+
